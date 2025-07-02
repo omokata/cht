086ffd09d6bcebc794480ba7b300641fc7cbb213
amin kafri <abeamin23@gmail.com>
2025-07-02 22:29:38 +0800

    able to count up to 25 capacity with naive approach

--- main.c
+++ main.c
@@ -1,31 +1,7 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <ctype.h>
-
-typedef struct {
-	char *key;
-	size_t val;
-} FreqKV;
-
-typedef struct {
-	FreqKV *items;
-	size_t count;
-	size_t capacity;
-} FreqKVs;
-
-int new_freq_kvs(FreqKVs *ht, size_t capacity)
-{
-	FreqKV *items = malloc(capacity*sizeof(FreqKV));
-	if (items == NULL) {
-		perror("ht");
-		return 0;
-	}
-	size_t count = 0;
-	ht->items = items;
-	ht->capacity = capacity;
-	ht->count = count;
-	return 1;
-}
 
 int tokenizer(FILE *stream, char word[])
 {
@@ -49,25 +25,79 @@ int tokenizer(FILE *stream, char word[])
 	}
 }
 
-int main()
+typedef struct {
+	char *key;
+	size_t val;
+} FreqKV;
+
+typedef struct {
+	FreqKV *items;
+	size_t count;
+	size_t cap;
+} FreqKVs;
+
+int new_freq_kvs(FreqKVs *ht, size_t capacity)
 {
-	FreqKVs freq_kv = {0};
-	if (!new_freq_kvs(&freq_kv, 25)) {
+	FreqKV *items = malloc(capacity*sizeof(FreqKV));
+	if (items == NULL) {
+		perror("ht");
+		return 0;
+	}
+	size_t count = 0;
+	ht->items = items;
+	ht->cap = capacity;
+	ht->count = count;
+	return 1;
+}
+
+void log_freq(FreqKVs ht)
+{
+	printf("HT Count: %d\n", ht.count);
+	for (size_t i = 0; i < ht.count; ++i) {
+		printf("%s => %d\n", ht.items[i].key, ht.items[i].val);
+	}
+}
+
+int main(void)
+{
+	FreqKVs ht = {0};
+	if (!new_freq_kvs(&ht, 25)) {
 		return 1;
 	}
 
-	printf("cap: %d, length: %d\n", freq_kv.capacity, freq_kv.count);
+	printf("cap: %d, length: %d\n", ht.cap, ht.count);
 	
 	const char *path = "t8.shakespeare.txt";
 	FILE *file = fopen(path, "r");
 
-	for (int i = 0; i < 1000; ++i) {
+	for (int i = 0; i < 5000; ++i) {
 		char word[256];
-	    if (tokenizer(file, word))
-			printf("%s\n", word);
+	    if (!tokenizer(file, word)) continue;
+		/* printf("Word: %s\n", word); */
+		for (int j = 0; j < ht.cap; ++j) {
+			/* printf("Val: %d\n", ht.items[j].val); */
+			/* printf("Word: %s\n", word); */
+			if (ht.items[j].val == 0) {
+				ht.items[j].key = malloc(16*sizeof(char));
+				strcpy(ht.items[j].key, word);
+				ht.items[j].val++;
+				ht.count++;
+				break;
+			} else {
+				if (strcmp(ht.items[j].key, word) == 0) {
+					ht.items[j].val++;
+					break;
+				} else {
+					continue;
+				}
+			}
+		}
+		if (ht.count >= ht.cap) break;
 	}
 
 	fclose(file);
+
+	log_freq(ht);
 	return 0;
 }
 

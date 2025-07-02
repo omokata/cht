bfaa9c083716c5a8a0ea6cea2b619e4679568be0
amin kafri <abeamin23@gmail.com>
2025-07-02 21:58:37 +0800

    initialize ht

--- main.c
+++ main.c
@@ -1,8 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <ctype.h>
+
+typedef struct {
+	char *key;
+	size_t val;
+} FreqKV;
+
+typedef struct {
+	FreqKV *items;
+	size_t count;
+	size_t capacity;
+} FreqKVs;
+
+int new_freq_kvs(FreqKVs *ht, size_t capacity)
+{
+	FreqKV *items = malloc(capacity*sizeof(FreqKV));
+	if (items == NULL) {
+		perror("ht");
+		return 0;
+	}
+	size_t count = 0;
+	ht->items = items;
+	ht->capacity = capacity;
+	ht->count = count;
+	return 1;
+}
 
 int tokenizer(FILE *stream, char word[])
 {
+
 	static char last_char = ' ';
 
 	while (isspace(last_char))
@@ -24,6 +51,13 @@ int tokenizer(FILE *stream, char word[])
 
 int main()
 {
+	FreqKVs freq_kv = {0};
+	if (!new_freq_kvs(&freq_kv, 25)) {
+		return 1;
+	}
+
+	printf("cap: %d, length: %d\n", freq_kv.capacity, freq_kv.count);
+	
 	const char *path = "t8.shakespeare.txt";
 	FILE *file = fopen(path, "r");
 
